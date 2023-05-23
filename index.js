8ad5098cadec23e18653155831db26b483167598
Denis Bessarab <99955098+Shmiterling@users.noreply.github.com>
2023-05-23 20:57:47 +0200

    adding nodejs server

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express'
 import { dirname } from 'path'
+import * as path from "path";
 
 const app = express()
 
 const __dirname = dirname('/')
 
-app.use(express.static(__dirname + 'dist/'))
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/', (req, res) => {
+    res.redirect('/index.html');
+});
+
 app.listen(process.env.PORT || 8080)
