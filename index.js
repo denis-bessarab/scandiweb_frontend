46a2def7cbd0eae1f35b196f8a78d99e04ddd21b
Denis Bessarab <99955098+Shmiterling@users.noreply.github.com>
2023-05-23 20:53:08 +0200

    adding nodejs server

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const app = express()
 
 const __dirname = dirname('/')
 
-app.use(express.static(__dirname + 'dist'))
-app.get('/')
+app.use(express.static(__dirname + 'dist/'))
 app.listen(process.env.PORT || 8080)
