b1e9a40016fdc159bb002965b07c260d3ffc449a
Denis Bessarab <99955098+Shmiterling@users.noreply.github.com>
2023-05-23 22:43:08 +0200

    adding nodejs server

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
 import * as path from 'path';
 
 const app = express();
 
+// Resolve the directory path of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Set the static file directory to serve the built Vue files
 const staticDir = path.join(__dirname, 'dist');
 app.use(express.static(staticDir));
 
+// Serve the index.html file for all routes
 app.get('*', (req, res) => {
     res.sendFile(path.join(staticDir, 'index.html'));
 });
+
 app.listen(process.env.PORT || 8080, () => {
     console.log('Server is running on port', process.env.PORT || 8080);
 });
