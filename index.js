6693d5e7b25687584cb49ed08eb9fabce9653f30
Denis Bessarab <99955098+Shmiterling@users.noreply.github.com>
2023-05-23 21:05:31 +0200

    adding nodejs server

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from 'express'
 import { dirname } from 'path'
 import * as path from "path";
+import * as fs from "fs";
 
 const app = express()
 
 const __dirname = dirname('/')
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/')));
 app.get('/', (req, res) => {
-    res.redirect('dist/index.html');
+    fs.readFile('dist/index.html',(err,data) => {
+        if(err) throw err
+        res.writeHead(200,{'Content-Type':'text/html'})
+        res.write(data)
+        res.end()
+        return res
+    })
 });
 
 app.listen(process.env.PORT || 8080)
